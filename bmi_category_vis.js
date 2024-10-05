888e11f25483bbca6bd8d37e394faa0d12716a07
mmio0004 <113961334+mmio0004@users.noreply.github.com>
2024-10-05 23:38:19 +0800

    add files

---
+++ bmi_category_vis.js
@@ -0,0 +1,7 @@
+var vg_2 = "stacked_area_chart_men_weight_categories.vg.json";
+
+vegaEmbed("#stacked_area", vg_2).then(function(result) {
+    
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
